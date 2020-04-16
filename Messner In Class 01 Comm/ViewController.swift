4183d206619b53a8aa018a0f96547c5eecafad60
Brad D Messner <bmessner@setonhill.edu>
2020-04-16 03:17:17 -0400

    Initial Commit

---
+++ Messner In Class 01 Comm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messner In Class 01 Comm
+//
+//  Created by Brad D. Messner on 4/16/20.
+//  Copyright © 2020 Brad D. Messner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
